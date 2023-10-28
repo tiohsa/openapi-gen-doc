e425ba580fb479eb793a1e1f3a9eda408320bec3
glorydays <glorydays11@gmail.com>
2023-10-29 00:26:58 +0900

    update

--- src/main/scala/generator/models/TestCase.scala
+++ src/main/scala/generator/models/TestCase.scala
@@ -1,5 +1,16 @@
 package generator.models
 
+val inputValues: Map[String, String] = Map(
+  ("tags" -> "tags_value"),
+  ("limit" -> "limit_value")
+)
+
+val outputValues: Map[String, String] = Map(
+  ("name" -> "name_value"),
+  ("tag" -> "tag_value"),
+  ("id" -> "id_value")
+)
+
 case class TestCase(
     method: String,
     path: String,
@@ -12,9 +23,16 @@ case class TestCase(
   override def toString: String = {
     this.responses
       .flatMap { response =>
-        List(titleString, givenString, whenString, thenString(response))
+        createTestCase(response)
       }
       .mkString("\n")
+  }
+
+  def createTestCase(response: TestResponse): List[String] = {
+    List(titleString, givenString, whenString, thenString(response))
+    //        if response.code == "200" then
+    //          List(titleString, givenString, whenString, thenString(response))
+    //        else Nil
   }
 
   def givenString: String =
@@ -42,13 +60,14 @@ case class TestCase(
 object TestCase {
 
   def parameterString(name: String, value: String): String =
-    s"${name}に\"${value}\"を設定する"
+    val input = inputValues.getOrElse(name, value)
+    s"${name}に\"${input}\"を設定する"
 
   def executionString(path: String, method: String): String =
     s"${method} ${path}にデータを送信する"
 
   def codeString(code: String): String =
-    s"ステータスコード${code}が返ること"
+    s"ステータスコード\"${code}\"が返ること"
 
   def responsesString(
       properties: List[TestProperty]
@@ -58,5 +77,6 @@ object TestCase {
     }
 
   def responseString(name: String, value: String): String =
-    s"${name}に${value}が返ること"
+    val output = outputValues.getOrElse(name, value)
+    s"${name}に\"${output}\"が返ること"
 }
