d259842ed2c5f8893e054fa83f2da77368515328
glorydays <glorydays11@gmail.com>
2023-10-29 01:53:30 +0900

    update

--- src/main/scala/generator/models/ReadCsv.scala
+++ src/main/scala/generator/models/ReadCsv.scala
@@ -1,7 +1,18 @@
 package generator.models
 
+import com.github.tototoshi.csv.CSVReader
+
+import java.io.File
+import scala.util.{Failure, Success, Try}
+
 object ReadCsv {
-  def read(path: String): Map[String, Map[String, String]]] = {
-    
+  def read(path: String): Map[String, Map[String, String]] = {
+    val reader = CSVReader.open(new File(path))
+    val csv = Try(reader.all())
+    csv match
+      case Success(c) => c
+      case Failure(e) => println(e)
+    reader.close()
   }
+  Map()
 }
